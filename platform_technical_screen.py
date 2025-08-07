ef84ba1becc804163bde9b1c3cbce7a696be5e00
K C <kc@Mac-mini.ht.home>
2025-08-07 14:52:03 -0800

    add edge cases

--- platform_technical_screen.py
+++ platform_technical_screen.py
@@ -2,12 +2,16 @@
 
 def sort(width, height, length, mass) -> str:
     """
-    :param width: width of the package, unit in centimeters
-    :param height: height of the package, unit in centimeters
-    :param length: length of the package, unit in centimeters
-    :param mass: weight of the package, unit in kilograms
+    :param width: width of the package, unit in centimeters, must be > 0
+    :param height: height of the package, unit in centimeters, must be > 0
+    :param length: length of the package, unit in centimeters, must be > 0
+    :param mass: weight of the package, unit in kilograms, must be > 0
     :return: name of the stack that package should go
     """
+    assert width > 0, "Package width must be > 0"
+    assert height > 0, "Package height must be > 0"
+    assert length > 0, "Package length must be > 0"
+    assert mass > 0, "Package mass must be > 0"
     bulky = 0
     heavy = 0
     volume = width * height * length
--- test.py
+++ test.py
@@ -4,12 +4,18 @@ from platform_technical_screen import sort
 
 class TestPackage(TestCase):
 
+    def test_invalid_input(self):
+        with self.assertRaises(ValueError):
+            sort(width=0, height=1, length=90, mass=19)
+        with self.assertRaises(ValueError):
+            sort(width=10, height=1, length=90, mass=0)
+
     def test_standard_output(self):
         label = sort(width=100, height=100, length=90, mass=19)
         self.assertTrue(label == "STANDARD")
 
-    def test_large_width(self):
-        label = sort(width=150, height=10, length=10, mass=10)
+    def test_large_height(self):
+        label = sort(width=50, height=150, length=10, mass=10)
         self.assertTrue(label == "SPECIAL")
 
     def test_large_volume(self):
@@ -21,7 +27,7 @@ class TestPackage(TestCase):
         self.assertTrue(label == "SPECIAL")
 
     def test_reject_output(self):
-        label = sort(width=10, height=150, length=100, mass=20)
+        label = sort(width=10, height=10, length=150, mass=50)
         self.assertTrue(label == "REJECTED")
 
 if __name__ == '__main__':

4f821b6be5e18757ec49dbb5e4d5afabbeb10101
K C <kc@Mac-mini.ht.home>
2025-08-07 14:56:43 -0800

    update readme

--- platform_technical_screen.py
+++ platform_technical_screen.py
@@ -2,6 +2,17 @@
 
 def sort(width, height, length, mass) -> str:
     """
+    Sort the packages using the following criteria:
+
+    - A package is bulky if its volume (Width x Height x Length) is greater than or equal to 1,000,000 cm³ or when one of its dimensions is greater or equal to 150 cm.
+    - A package is heavy when its mass is greater or equal to 20 kg.
+
+    Dispatch the packages in the following stacks:
+    - STANDARD: standard packages (those that are not bulky or heavy) can be handled normally.
+    - SPECIAL: packages that are either heavy or bulky can't be handled automatically.
+    - REJECTED: packages that are **both** heavy and bulky are rejected.
+
+
     :param width: width of the package, unit in centimeters, must be > 0
     :param height: height of the package, unit in centimeters, must be > 0
     :param length: length of the package, unit in centimeters, must be > 0
